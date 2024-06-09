cbbf4f9efda9baef998e755e6ba75c30f650a65f
aremumatthew <aremumatthew112@gmail.com>
2024-06-09 16:59:44 +0000

    more updates

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+let openEl = document.querySelector('.closebtn');
+let projectOpen = document.getElementById('svg');
+let projectclose = document.getElementById('close-svg');
+let projectElement = document.querySelector('.project-details')
 function openNav(){
   document.getElementById("mySidenav").style.width = "100%";
+  openEl.style.opacity = "1";
+
 }
+
 
 function closeNav(){
   document.getElementById("mySidenav").style.width = "0";
+  openEl.style.opacity = "0";
+}
+
+for (let i = 1; i < projectOpen.length; i++){
+  projectOpen[i].addEventListener("click", function(){
+    alert("hi")
+  })
 }
