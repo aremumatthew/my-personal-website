deb2597c8eb5ffbbfe1f0471924392e26f7dcf25
aremumatthew <aremumatthew112@gmail.com>
2025-03-03 02:07:36 +0000

    more edits

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
+
+projectOpen.forEach((item)=>{
 let openEl = document.querySelector('.closebtn');
 let projectOpen = document.getElementById('svg');
 let projectclose = document.getElementById('close-svg');
 let projectElement = document.querySelector('.project-details')
+
+  projectOpen[item].addEventListener('click', ()=>{
+  alert('hi')
+  })
+})
+
+
+
+const img = document.querySelector('.profileImg')
+const header = document.querySelector('.header');
+const openNavBtn = document.querySelector('.openbtn');
+openNavBtn.addEventListener('click', openNav);
+
+const closeNavBtn = document.querySelector('.closebtn');
+closeNavBtn.addEventListener('click', closeNav);
+
+function onScroll (){
+  if (scrollY > 144 ){
+    header.style.display = 'none'
+  }else {
+    header.style.display = 'flex'
+  }
+
+}
+
 function openNav(){
   document.getElementById("mySidenav").style.width = "100%";
-  openEl.style.opacity = "1";
-
+  closeNavBtn.style.opacity = "1";
 }
 
 
 function closeNav(){
   document.getElementById("mySidenav").style.width = "0";
-  openEl.style.opacity = "0";
+  closeNavBtn.style.opacity = '0';
 }
-
-for (let i = 1; i < projectOpen.length; i++){
-  projectOpen[i].addEventListener("click", function(){
-    alert("hi")
-  })
-}
+window.addEventListener('scroll', onScroll)
